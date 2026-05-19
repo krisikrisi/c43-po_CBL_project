84f7b07826b027798c5a59429e5d9a8ec969d2e7
Monika <evonika06@yandex.ru>
2026-05-19 09:07:41 +0200

    moving btw cells

---
+++ my_robot_controller/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_robot_controller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='ubuntu@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'move_to_cell = my_robot_controller.move_to_cell:main', # create command "move to cell". 1) packet, 2) file, 3) function main
+        ],
+    },
+)
